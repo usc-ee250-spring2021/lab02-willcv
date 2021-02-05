22362de6dfb16c73d9ce2c35dd421e59f328a104
Will Volpe <wvolpe@usc.edu>
2021-02-04 19:01:36 -0800

    Lab02

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -23,10 +23,16 @@ sys.path.append('../../Software/Python/')
 sys.path.append('../../Software/Python/grove_rgb_lcd')
 
 import grovepi
+from grove_rgb_lcd import *
 
 """This if-statement checks if you are running this python file directly. That 
 is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will 
 be true"""
+
+potentiometer = 0
+ultrasonic_ranger = 4
+
+
 if __name__ == '__main__':
     PORT = 4    # D4
 
@@ -35,4 +41,12 @@ if __name__ == '__main__':
         #sleep for a reasonable time of 200ms between each iteration.
         time.sleep(0.2)
 
-        print(grovepi.ultrasonicRead(PORT))
+        threshold = grovepi.analogRead(potentiometer)
+        distance = grovepi.ultrasonicRead(PORT)
+
+        if distance >= threshold:
+            setText_norefresh(str(threshold) + "cm          \n" + str(distance) + "cm")
+            setRGB(0,255,0)
+        else:
+            setText_norefresh(str(threshold) + "cm OBJ PRES \n" + str(distance) + "cm")
+            setRGB(255,0,0)
